9e1e0f9d444fa5f98ef50646a2de979eadaabfa4
shan1611 <shantudhami@gmail.com>
2026-05-18 22:33:49 +0530

    silver layer cleaned and loaded

--- scripts/create_silver_tables.sql
+++ scripts/create_silver_tables.sql
@@ -85,3 +85,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+
+
+
---
+++ scripts/silver_load_clean.sql
@@ -0,0 +1,238 @@
+
+
+-- Loading silver.crm_cust_info
+create or alter procedure silver.load_silver as
+
+begin
+
+begin try 
+
+--start loading tables
+
+declare @start_time datetime,@end_time datetime,@batch_start_time datetime,@batch_end_time datetime;
+    set @batch_start_time=getdate();
+    set @start_time= getdate();
+    print '>> Truncating Table: sivler.crm_cust_info:'
+    Truncate table silver.crm_cust_info;
+    print '>> Inserting Into: sivler.crm_cust_info:'
+INSERT INTO silver.crm_cust_info (
+			cst_id, 
+			cst_key, 
+			cst_firstname, 
+			cst_lastname, 
+			cst_marital_status, 
+			cst_gndr,
+			cst_create_date
+		)
+SELECT
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE 
+				WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_marital_status, -- Normalize marital status values to readable format
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
+			cst_create_date
+		FROM (
+			SELECT
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+		) t
+	
+set @end_time= getdate();
+PRINT '>> LOAD DURATION: '+ CAST(DATEDIFF(second,@start_time,@end_time) as NVARCHAR(50))+' seconds';
+PRINT '==============================='
+
+-- Loading silver.crm_prd_info
+set @start_time= getdate();       
+print '>> Truncating Table: sivler.crm_prd_info:'
+Truncate table silver.crm_prd_info
+print '>> Inserting Into: sivler.crm_prd_info:'
+		INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost,
+			CASE 
+				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+				ELSE 'n/a'
+			END AS prd_line, -- Map product line codes to descriptive values
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt -- Calculate end date as one day before the next start date
+		FROM bronze.crm_prd_info;
+     
+set @end_time= getdate();
+PRINT '>> LOAD DURATION: '+ CAST(DATEDIFF(second,@start_time,@end_time) as NVARCHAR(50))+' seconds';
+PRINT '==============================='
+
+
+--insert into silver.crm_sales_details
+
+set @start_time= getdate();
+print '>> Truncating Table: sivler.crm_sales_details:'
+Truncate table silver.crm_sales_details
+print '>> Inserting Into: sivler.crm_sales_details:'
+insert into silver.crm_sales_details (
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales ,
+    sls_quantity,
+    sls_price)
+select 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+
+
+case when sls_order_dt is null or len(sls_order_dt)!=8 then null
+else cast(cast(sls_order_dt as varchar) as date)
+end as sls_order_dt,
+
+case when sls_ship_dt is null or len(sls_ship_dt)!=8 then null
+else cast(cast(sls_ship_dt as varchar) as date)
+end as sls_ship_dt,
+
+case when sls_due_dt is null or len(sls_due_dt)!=8 then null
+else cast(cast(sls_due_dt as varchar) as date)
+end as sls_due_dt,
+
+case when sls_sales is null or sls_sales <=0 or sls_sales != sls_quantity * abs(sls_price)
+then sls_quantity * abs(sls_price)
+else sls_sales
+end as sls_sales,
+
+case when sls_quantity is null or sls_quantity <=0
+then sls_sales/sls_price
+else sls_quantity
+end as sls_quantity,
+
+case when sls_price is null or sls_price <=0
+then sls_sales/sls_quantity
+else sls_price
+end as sls_price
+
+from bronze.crm_sales_details;
+
+set @end_time= getdate();
+PRINT '>> LOAD DURATION: '+ CAST(DATEDIFF(second,@start_time,@end_time) as NVARCHAR(50))+' seconds';
+PRINT '==============================='
+
+--insert into silver.erp_cust
+
+set @start_time= getdate();
+print '>> Truncating Table: sivler.erp_cust_az12:'
+Truncate table silver.erp_cust_az12
+print '>> Inserting Into: sivler.erp_cusr_az12:'
+
+insert into silver.erp_cust_az12(
+cid,bdate,gen
+)
+select 
+case when cid like '%NAS%' then substring(cid,4,len(4)) 
+else cid 
+end as cid,
+
+case when bdate>getdate() then NULL
+else bdate
+end as bdate,
+
+case when upper(trim(gen)) in ('F','FEMALE') then 'Female'
+when upper(trim(gen)) in ('M','MALE') then 'Male'
+else 'n/a'
+end as gen
+
+from bronze.erp_cust_az12;
+set @end_time= getdate();
+PRINT '>> LOAD DURATION: '+ CAST(DATEDIFF(second,@start_time,@end_time) as NVARCHAR(50))+' seconds';
+PRINT '==============================='
+
+
+--inert into silver.erp_loc
+set @start_time= getdate();
+print '>> Truncating Table: sivler.erp_loc_a101:'
+Truncate table silver.erp_loc_a101
+print '>> Inserting Into: sivler.erp_loc_a101:'
+insert into silver.erp_loc_a101 (cid,cntry)
+	select 
+	replace(cid,'-','') as cid,
+
+	case when trim(cntry) in ('US','USA') then 'United States'
+		when trim(cntry) = 'DE' then 'Germany'
+		when trim(cntry)='' or trim(cntry) is NULL then 'n/a'
+		else trim(cntry)
+     end as cntry
+from bronze.erp_loc_a101;
+set @end_time= getdate();
+PRINT '>> LOAD DURATION: '+ CAST(DATEDIFF(second,@start_time,@end_time) as NVARCHAR(50))+' seconds';
+PRINT '==============================='
+
+--insert into silver.erp_cat
+ set @start_time= getdate();
+print '>> Truncating Table: sivler.erp_px_cat_g1v2o:'
+Truncate table silver.erp_px_cat_g1v2
+print '>> Inserting Into: sivler.erp_px_cat_g1v2o:'
+
+insert into silver.erp_px_cat_g1v2 (
+id,
+cat,
+subcat,
+maintenance) 
+select 
+id,
+cat,
+subcat,
+maintenance
+
+from bronze.erp_px_cat_g1v2;
+
+set @end_time= getdate();
+PRINT '>> LOAD DURATION: '+ CAST(DATEDIFF(second,@start_time,@end_time) as NVARCHAR(50))+' seconds';
+PRINT '==============================='
+
+set @batch_end_time=getdate();
+PRINT 'SILVER LAYER LOAD DUARATION: '+CAST(DATEDIFF(second,@batch_start_time,@batch_end_time) as NVARCHAR(50))+' seconds';
+
+end try
+
+begin catch
+	PRINT '=========================================='
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+	PRINT 'Error Message' + ERROR_MESSAGE();
+	PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+	PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+	PRINT '=========================================='
+end catch
+
+
+
+end
