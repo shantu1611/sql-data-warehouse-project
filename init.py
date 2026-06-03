b8729b602bf4c1dd60fac67f3cfda75949e180da
Tshantu <shantudhami@gmail.com>
2026-06-04 00:26:49 +0530

    init.py

--- init.py
+++ init.py
@@ -3,19 +3,47 @@ import numpy as np
 import pandas as pd
 import time
 import seaborn as sns
+import pyodbc
 import matplotlib.pyplot as plt
-conn = connect("Server=SHAN\SQLEXPRESS;Database=DataWarehouse;Encrypt=yes;TrustServerCertificate=yes;Trusted_Connection=yes")
-
-SQL_QUERY = """
-SELECT age from gold.dim_customers where birthdate is not null;
-"""
-
+import sqlparse
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=SHAN\\SQLEXPRESS;"
+    "DATABASE=master;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=yes;"
+    "Trusted_Connection=yes;"
+)
+conn.autocommit = True
 cursor = conn.cursor()
-cursor.execute(SQL_QUERY)
 
-rows = cursor.fetchall()
-df=pd.DataFrame(rows)
-df.to_csv("ages.csv",index=False)
+cursor.execute("IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'DataWarehouse') CREATE DATABASE DataWarehouse")
+print("✓ Database created.")
+conn.close()
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=SHAN\\SQLEXPRESS;"
+    "DATABASE=master;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=yes;"
+    "Trusted_Connection=yes;"
+)
+conn.autocommit = True
+cursor = conn.cursor()
+
+with open("scripts/init.sql", "r", encoding="utf-8") as file:
+    sql_script = file.read()
+
+statements = sqlparse.split(sql_script)
+for statement in statements:
+    if statement.strip():
+        cursor.execute(statement)
+print(sql_script)
+
+# rows = cursor.fetchall()
+# df=pd.DataFrame(rows)
+# df.to_csv("tables.csv",index=False)
+# print(df.head())
 # fig, ax = plt.subplots(1,2, figsize=(18, 5))
 # x = (df - df.mean())/df.std()
 # sns.histplot(df,ax=ax[0])
