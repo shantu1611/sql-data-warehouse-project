b5b4987210ee1ef0fde2632a997e92c6726ab04b
shan1611 <shantudhami@gmail.com>
2026-06-03 23:04:40 +0530

    init

--- init.py
+++ init.py
@@ -2,6 +2,8 @@ from mssql_python import connect
 import numpy as np
 import pandas as pd
 import time
+import seaborn as sns
+import matplotlib.pyplot as plt
 conn = connect("Server=SHAN\SQLEXPRESS;Database=DataWarehouse;Encrypt=yes;TrustServerCertificate=yes;Trusted_Connection=yes")
 
 SQL_QUERY = """
@@ -12,6 +14,10 @@ cursor = conn.cursor()
 cursor.execute(SQL_QUERY)
 
 rows = cursor.fetchall()
-
-
-print(len(rows))
+df=pd.DataFrame(rows)
+df.to_csv("ages.csv",index=False)
+# fig, ax = plt.subplots(1,2, figsize=(18, 5))
+# x = (df - df.mean())/df.std()
+# sns.histplot(df,ax=ax[0])
+# sns.histplot(x,ax=ax[1])
+# plt.show()
